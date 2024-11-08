14ae576362d867f54548f4b89f0f949adee1fe65
Grimmjow <samy.tchokounte@universite-paris-saclay.fr>
2024-11-08 14:09:19 +0100

    Vanesssssssaaaaaaaaaaaaaa

---
+++ Jeux.h
@@ -0,0 +1,24 @@
+//
+// Created by Asus on 08/11/2024.
+//
+
+#ifndef JEUX_H
+#define JEUX_H
+#include "Player.h"
+#include "vector"
+#include "Reserve.h"
+
+
+class Jeux {
+private:
+    std::vector<Reserve> m_reserve;
+    std::vector<Player> m_players;
+    Player m_actif;
+public:
+
+
+};
+
+
+
+#endif //JEUX_H
